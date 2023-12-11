40d261d57918f433918707e69ffba804ce79dd26
Dominic20227 <dominicmalloy2@outlook.com>
2023-12-11 14:37:16 +0000

    amended based on feedback

--- src/components/ArticleDetail.jsx
+++ src/components/ArticleDetail.jsx
@@ -1,12 +1,16 @@
+import dayjs from "dayjs";
+
 function ArticleDetail({ singleArticle }) {
+  const date = String(dayjs(singleArticle.created_at).$d);
+
   return (
     <div className="article-card">
       <h3>Article detail</h3>
       <li>
         <h4>{singleArticle.title}</h4>
         <p>{singleArticle.body}</p>
-        <p>author: {singleArticle.author}</p>
-        <p>Date: {singleArticle.created_at}</p>
+        <p> {singleArticle.author}</p>
+        <p> {date}</p>
         <p>Votes: {singleArticle.votes}</p>
       </li>
     </div>
