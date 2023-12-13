73a8a8fa4dcfbe3379d783a6c4d40f259b0e627f
Dominic20227 <dominicmalloy2@outlook.com>
2023-12-13 16:27:07 +0000

    deleting unused functions

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -30,58 +30,6 @@ function Articles() {
     return <p>Loading</p>;
   }
 
-  const sortTopicsByVotes = () => {
-    const newArticles = [...articles];
-
-    newArticles.sort(function (a, b) {
-      if (votesFilter === "dec") {
-        setVotesFilter("asc");
-        var keyA = new Date(a.votes),
-          keyB = new Date(b.votes);
-        // Compare the 2 dates
-        if (keyA < keyB) return -1;
-        if (keyA > keyB) return 1;
-        return 0;
-      } else {
-        setVotesFilter("dec");
-        var keyC = new Date(a.votes),
-          keyD = new Date(b.votes);
-        // Compare the 2 dates
-        if (keyC < keyD) return 1;
-        if (keyC > keyD) return -1;
-        return 0;
-      }
-    });
-
-    setArticles(newArticles);
-  };
-
-  const sortTopicsByComments = () => {
-    const newArticles = [...articles];
-
-    newArticles.sort(function (a, b) {
-      if (CommentsFilter === "dec") {
-        setCommentsFilter("asc");
-        var keyA = new Date(a.comment_count),
-          keyB = new Date(b.comment_count);
-        // Compare the 2 dates
-        if (keyA < keyB) return -1;
-        if (keyA > keyB) return 1;
-        return 0;
-      } else {
-        setCommentsFilter("dec");
-        var keyC = new Date(a.comment_count),
-          keyD = new Date(b.comment_count);
-        // Compare the 2 dates
-        if (keyC < keyD) return 1;
-        if (keyC > keyD) return -1;
-        return 0;
-      }
-    });
-
-    setArticles(newArticles);
-  };
-
   return (
     <div>
       <Navbar />
