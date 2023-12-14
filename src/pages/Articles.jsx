b4797687c62ee2608551b7014eaa4d284a98ab79
Dominic20227 <dominicmalloy2@outlook.com>
2023-12-14 11:41:14 +0000

    added sort options on articles page

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -8,9 +8,7 @@ import { SortByComments, SortByDate, SortByVotes } from "../utils/utils";
 function Articles() {
   const [articles, setArticles] = useState("");
   const [hasLoaded, setHasLoaded] = useState(false);
-  const [dateFilter, setDateFilter] = useState("dec");
-  const [votesFilter, setVotesFilter] = useState("dec");
-  const [commentsFilter, setCommentsFilter] = useState("dec");
+
   const { topic } = useParams();
 
   useEffect(() => {
@@ -33,36 +31,53 @@ function Articles() {
   return (
     <div>
       <Navbar />
-      <button
-        onClick={() => {
-          SortByDate(articles, setArticles, dateFilter, setDateFilter);
-        }}
-      >
-        Sort by date
-      </button>
 
-      <button
-        onClick={() =>
-          SortByVotes(articles, setArticles, votesFilter, setVotesFilter)
-        }
-      >
-        Sort by Votes {votesFilter}
-      </button>
+      <p>
+        Sort Votes
+        <select
+          onChange={(e) => {
+            const userSelection = e.target.value;
 
-      <button
-        onClick={() =>
-          SortByComments(
-            articles,
-            setArticles,
-            commentsFilter,
-            setCommentsFilter
-          )
-        }
-      >
-        Sort by Comments {commentsFilter}
-      </button>
+            SortByVotes(userSelection, articles, setArticles);
+          }}
+        >
+          <option> Choose</option>
+          <option value="ASC">ASC</option>
+          <option value="DEC">DEC</option>
+        </select>
+      </p>
+
+      <p>
+        Sort Dates
+        <select
+          onChange={(e) => {
+            const userSelection = e.target.value;
+
+            SortByDate(userSelection, articles, setArticles);
+          }}
+        >
+          <option> Choose</option>
+          <option value="ASC">ASC</option>
+          <option value="DEC">DEC</option>
+        </select>
+      </p>
+
+      <p>
+        Sort Comments
+        <select
+          onChange={(e) => {
+            const userSelection = e.target.value;
+
+            SortByComments(userSelection, articles, setArticles);
+          }}
+        >
+          <option> Choose</option>
+          <option value="ASC">ASC</option>
+          <option value="DEC">DEC</option>
+        </select>
+      </p>
+
       <h2>Articles </h2>
-
       <ul>
         <ul>
           {articles.map((article) => {
--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,25 +1,21 @@
-export const SortByDate = (
-  articles,
-  setArticles,
-  dateFilter,
-  setDateFilter
-) => {
+export const SortByDate = (userSelection, articles, setArticles) => {
   const newArticles = [...articles];
 
   newArticles.sort(function (a, b) {
-    if (dateFilter === "dec") {
-      setDateFilter("asc");
+    if (userSelection === "ASC") {
       var keyA = new Date(a.created_at),
         keyB = new Date(b.created_at);
       // Compare the 2 dates
+
       if (keyA < keyB) return -1;
       if (keyA > keyB) return 1;
       return 0;
-    } else {
-      setDateFilter("dec");
+    }
+    if (userSelection === "DEC") {
       var keyC = new Date(a.created_at),
         keyD = new Date(b.created_at);
       // Compare the 2 dates
+
       if (keyC < keyD) return 1;
       if (keyC > keyD) return -1;
       return 0;
@@ -29,28 +25,24 @@ export const SortByDate = (
   setArticles(newArticles);
 };
 
-export const SortByVotes = (
-  articles,
-  setArticles,
-  votesFilter,
-  setVotesFilter
-) => {
+export const SortByVotes = (userSelection, articles, setArticles) => {
   const newArticles = [...articles];
 
   newArticles.sort(function (a, b) {
-    if (votesFilter === "dec") {
-      setVotesFilter("asc");
-      var keyA = new Date(a.created_at),
-        keyB = new Date(b.created_at);
+    if (userSelection === "ASC") {
+      var keyA = a.votes,
+        keyB = b.votes;
       // Compare the 2 dates
+
       if (keyA < keyB) return -1;
       if (keyA > keyB) return 1;
       return 0;
-    } else {
-      setVotesFilter("dec");
-      var keyC = new Date(a.created_at),
-        keyD = new Date(b.created_at);
+    }
+    if (userSelection === "DEC") {
+      var keyC = a.votes,
+        keyD = b.votes;
       // Compare the 2 dates
+
       if (keyC < keyD) return 1;
       if (keyC > keyD) return -1;
       return 0;
@@ -59,29 +51,24 @@ export const SortByVotes = (
 
   setArticles(newArticles);
 };
-
-export const SortByComments = (
-  articles,
-  setArticles,
-  commentsFilter,
-  setCommentsFilter
-) => {
+export const SortByComments = (userSelection, articles, setArticles) => {
   const newArticles = [...articles];
 
   newArticles.sort(function (a, b) {
-    if (commentsFilter === "dec") {
-      setCommentsFilter("asc");
-      var keyA = new Date(a.created_at),
-        keyB = new Date(b.created_at);
+    if (userSelection === "ASC") {
+      var keyA = a.comment_count,
+        keyB = b.comment_count;
       // Compare the 2 dates
+
       if (keyA < keyB) return -1;
       if (keyA > keyB) return 1;
       return 0;
-    } else {
-      setCommentsFilter("dec");
-      var keyC = new Date(a.created_at),
-        keyD = new Date(b.created_at);
+    }
+    if (userSelection === "DEC") {
+      var keyC = a.comment_count,
+        keyD = b.comment_count;
       // Compare the 2 dates
+
       if (keyC < keyD) return 1;
       if (keyC > keyD) return -1;
       return 0;
